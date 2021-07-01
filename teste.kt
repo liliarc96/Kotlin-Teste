972c4c601a2e6a8f143b2fe33fe80ba4f0903358
Lília Cantalice <26139056+liliarc96@users.noreply.github.com>
2021-07-01 09:42:43 -0300

    Update teste.kt

--- teste.kt
+++ teste.kt
@@ -18,4 +18,35 @@ fun main() {
     //funções de ordem superior
     val x = calculate(12,4,::sum) //12 + 4
     val y = calculate(12,4){a,b-> a*b} // 12*4
+    
+    fun calculate(n1:Int,n2:Int,operation:(Int,Int)->Int):Int{
+        val result = operation(n1,n2)
+        return result
+    }
+    
+    fun sum(a1:Int,a2:Int) = a1.plus(a2)
+    
+    //funções & extensões
+    fun String.randomCapitalizedLetter() = this[(0..this.length-1).random()].toUpperCase()
+    
+    //estruturas de controle
+    if(umaVariavel > 0){
+        println("$umaVariavel é maior que zero!")
+    }else if(umaVariavel = 0){
+        println("$umaVariavel é igual que zero!")
+    }else{
+        println("$umaVariavel é menor que zero!")
+    }
+    
+    when{
+        case1 -> {}
+        case2 -> {}
+        case3 -> {}
+        else -> {}
+    }
+    
+    val a:Int? = null
+    var number = a ?: 0
+    
+    https://www.casasbahia.com.br/monitor-led-21-5-aoc-full-hd-22b1h-com-conexao-hdmi-e-base-ajustavel-13937084/p/13937084?utm_medium=Cpc&utm_source=GP_PLA&IdSku=13937084&idLojista=10037&utm_campaign=elte_smart-shopping&gclid=EAIaIQobChMInOC7pPDB8QIV4xmtBh1BhwRmEAQYEyABEgI95vD_BwE
 }
